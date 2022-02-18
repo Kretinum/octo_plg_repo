017471defeb22619e5407cffc026255a7bba1821
Kretinum <63955452+Kretinum@users.noreply.github.com>
2022-02-18 14:14:04 +0200

    testing version

--- octoprint_octo_plugin_dist/__init__.py
+++ octoprint_octo_plugin_dist/__init__.py
@@ -8,7 +8,7 @@ class HelloWorldPlugin(octoprint.plugin.StartupPlugin,octoprint.plugin.TemplateP
 
 
 __plugin_name__ = "conn_plugin"
-__plugin_version__ = "1.0.0"
+__plugin_version__ = "1.0.1"
 __plugin_description__ = ""
 __plugin_pythoncompat__ = ">=2.7,<4"
 __plugin_implementation__ = HelloWorldPlugin()

7aef2e58d20a014a47303a67119f2b5d6b1d2acd
Kretinum <63955452+Kretinum@users.noreply.github.com>
2022-02-27 13:47:21 +0200

    Connecting to server

--- octoprint_octo_plugin_dist/__init__.py
+++ octoprint_octo_plugin_dist/__init__.py
@@ -1,8 +1,16 @@
 from __future__ import absolute_import, unicode_literals
 
 import octoprint.plugin
+import socket
+
+
 
 class HelloWorldPlugin(octoprint.plugin.StartupPlugin,octoprint.plugin.TemplatePlugin):
+    def __init__(self):
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._socket(('192.168.1.103',42069))
+        self._socket.send("TEST_PRINTER")
+
     def on_after_startup(self):
         self._logger.info("TESTING")
 

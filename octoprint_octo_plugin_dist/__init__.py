3e27f4078151ae9efee4c1fc47d622e01e796b70
Kretinum <63955452+Kretinum@users.noreply.github.com>
2022-03-19 20:10:41 +0200

    maybe something is up

--- octoprint_octo_plugin_dist/__init__.py
+++ octoprint_octo_plugin_dist/__init__.py
@@ -3,14 +3,14 @@ from __future__ import absolute_import, unicode_literals
 import octoprint.plugin
 import socket
 from os.path import exists
-import client
+from client import client
 
 class HelloWorldPlugin(octoprint.plugin.StartupPlugin,octoprint.plugin.TemplatePlugin):
 
 
     def on_after_startup(self):
         self._logger.info("TESTING")
-        #self.__client = client()
+        self.__client = client()
         self._logger.info("STARTED CLIENT")
 
 

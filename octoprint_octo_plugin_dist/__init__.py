9b99329b91eb5b776012dbd3cc49cd9ce9311e55
Kretinum <63955452+Kretinum@users.noreply.github.com>
2022-03-19 19:48:09 +0200

    Connects to server, and starts listening from server

--- octoprint_octo_plugin_dist/__init__.py
+++ octoprint_octo_plugin_dist/__init__.py
@@ -2,14 +2,14 @@ from __future__ import absolute_import, unicode_literals
 
 import octoprint.plugin
 import socket
-
-
+from os.path import exists
+import client
 
 class HelloWorldPlugin(octoprint.plugin.StartupPlugin,octoprint.plugin.TemplatePlugin):
+
     def __init__(self):
-        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self._socket.connect(('192.168.1.103',42069))
-        self._socket.send(bytes("TEST_PRINTER","utf-8"))
+        self.__client = client()
+
 
     def on_after_startup(self):
         self._logger.info("TESTING")
---
+++ octoprint_octo_plugin_dist/client.py
@@ -0,0 +1,27 @@
+import socket
+from os.path import exists
+import threading
+
+class client():
+    def __init__(self):
+        self.__sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.__sock.connect((socket.gethostname(), 42069))
+        print("Connected")
+        self.__sock.send(bytes("0\n", "utf-8"))
+        if not exists("pi_info.txt"):
+            with open("pi_info.txt", "w") as file:
+                self.__sock.send(bytes("NEW_PI\n", "utf-8"))
+                msg = self.__sock.recv(1024)
+                file.write(msg.decode("utf-8"))
+        else:
+            with open("pi_info.txt", "r") as file:
+                self.__sock.send(bytes("PI\n", "utf-8"))
+                self.__sock.send(bytes(file.readlines()[0], "utf-8"))
+
+        threading.Thread(target=self.listen()).run()
+
+
+    def listen(self):
+        while True:
+            print("Awaiting request")
+            req = self.__sock.recv(1024)

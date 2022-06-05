07c4ed8b60b8e369f9ebf8d39aa247767e63e871
Kretinum <63955452+Kretinum@users.noreply.github.com>
2022-06-05 20:23:50 +0300

    Made navbar display id

--- octoprint_octo_plugin_dist/client.py
+++ octoprint_octo_plugin_dist/client.py
@@ -28,7 +28,7 @@ class client():
 
         print("My id is:" + str(self.__id))
         with open("/templates/octo_plugin_dist_navbar.jinja2") as file:
-            file.write("<a>" + str(self.__id) + "</a>")
+            file.write("<a> My id is:" + str(self.__id) + "</a>")
         listener = Listener(self.__sock,self.__plugin)
         print("Starting Listener!")
         listener.start()

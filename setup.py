7ba3bc307f2456381c0703586d75a63df0cae518
Kretinum <63955452+Kretinum@users.noreply.github.com>
2022-06-07 22:57:38 +0300

    Adding frequency setting

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ plugin_package = "octoprint_reprint3d"
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
-plugin_name = "reprint3d"
+plugin_name = "OctoPrint-REPrint3D"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
 plugin_version = "0.1.1"

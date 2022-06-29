7328c72067862893c61c92e6ff8c94360d74f280
Fuia Mihai <mihai.fuia@gmail.com>
2022-06-30 01:02:40 +0300

    debug stuff

--- octoprint_reprint3d/client.py
+++ octoprint_reprint3d/client.py
@@ -53,7 +53,7 @@ class Listener(threading.Thread):
             req_len = int.from_bytes(self.__sock.recv(4), "big")
             req = self.__sock.recv(req_len)
             req_type = req.decode("utf-8")
-            print(bytes(req_type,"utf-8"))
+            self.__plugin._logger(bytes(req_type,"utf-8"))
             if req_type == "ADD_GCODE":
                 len = int.from_bytes(self.__sock.recv(4),"big")
                 fileName = bytes.decode(self.__sock.recv(len),"utf-8");
